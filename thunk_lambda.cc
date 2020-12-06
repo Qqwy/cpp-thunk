9fd092910c76650799e3ed60bd840b436bde35bd
Marten/Qqwy <w-m@wmcode.nl>
2020-12-06 22:18:41 +0100

    cleaning stuff up

--- thunk_lambda.cc
+++ thunk_lambda.cc
@@ -20,11 +20,12 @@ template<
 
 public:
 
-  Thunk(const Thunk&) = default;
-  Thunk(Thunk &&) = default;
   constexpr Thunk(Lambda const &lambda) noexcept :
     d_thunk{lambda}
   {}
+
+  Thunk(const Thunk&) = default;
+  Thunk(Thunk &&) = default;
 
   constexpr operator Result() const noexcept(noexcept(std::declval<Lambda>())) {
     // Morally correct const_cast
@@ -44,12 +45,13 @@ private:
   constexpr Result visit() noexcept(noexcept(std::declval<Lambda>())) {
     return std::visit([this](auto &&arg) {
       using T = std::decay_t<decltype(arg)>;
-      if constexpr(std::is_same_v<Result, T>) {
-        return arg;
-      } else if constexpr(std::is_same_v<Lambda, T>){
+
+      if constexpr(std::is_same_v<Lambda, T>){
         Result res = arg();
         d_thunk.template emplace<Result>(res);
         return res;
+      } else if constexpr(std::is_same_v<Result, T>) {
+          return arg;
       } else {
         static_assert(always_false_v<T>, "non-exhaustive visitor!");
       }
@@ -64,8 +66,16 @@ template<typename F>auto constexpr make_thunk(F lambda) -> Thunk<F> {
   return Thunk<F>{lambda};
 }
 
+template<typename Lam,
+         typename Res = std::invoke_result_t<Lam>
+         > std::ostream &operator<<(std::ostream &out, Thunk<Lam> const &val)
+{
+  return out << (val.operator Res());
+};
+
+
 int main(){
   auto res = make_thunk([&]{ return std::string{"Hello, world!"}; });
-  std::string evaluated = res;
-  std::cout << evaluated << '\n';
+  // std::string evaluated = res;
+  std::cout << res << '\n';
 }
