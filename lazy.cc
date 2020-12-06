4cb0f7fd0d09844ea0428327aaf1ecb8a70a3259
Marten/Qqwy <w-m@wmcode.nl>
2020-12-06 23:56:31 +0100

    remove thunk code from lazy source files.

--- lazy.cc
+++ lazy.cc
@@ -75,49 +75,6 @@ template<typename F>auto constexpr operator+(LazyProxy const &, F lambda) -> Laz
 
 #define lazy LazyProxy{} + [&]
 
-
-
-
-// A Thunk.
-// Similar to `Lazy`, but will only compute the result once.
-// Idea: Calling `d_fun` the first time will evaluate `lambda` but then replace `d_fun` with a trivial lambda
-// which does not do any more work but simply returns the previous result.
-// (Requires Result to be copyable for obvious reasons).
-template<typename Result>class Thunk
-{
-  std::function<Result()> d_lambda;
-
-public:
-  Thunk(std::function<Result()> lambda):
-    d_lambda([=, this]{
-      Result res = lambda();
-      d_lambda = [=]{ return res; };
-      return res;
-    })
-  {}
-
-  Thunk(const Thunk &) = default;
-  Thunk(Thunk &&) = default;
-
-  constexpr operator Result() const {
-    return d_lambda();
-  }
-
-  // a Thunk is convertible to everything Result is convertible to.
-  // (fixing the 'single conversion' problem)
-  // 'conceptually' const, but secretly the initial call to `d_lambda` modifies d_lambda itself.
-  template<typename T>constexpr operator T() const
-  {
-    return static_cast<T>(d_lambda());
-  }
-};
-
-// Helpers for `Thunk` can be made in a similar vein as was done for `Lazy`.
-template<typename Result> std::ostream &operator<<(std::ostream &out, Thunk<Result> const &val) {
-  return out << (val.operator Result());
-};
-
-
 int main()
 {
     auto res = make_lazy([]{ return std::string("Hello world!\n");});
@@ -134,13 +91,6 @@ int main()
     int sum = (fourtytwo + sixtynine);
 
     std::cout << "The result is: " << sum << '\n';
-
-    auto difficult_computation = Thunk<std::string>{[&]{
-      std::cout << "Performing work!\n";
-      return "Result Computed!\n";
-    }};
-
-    std::cout << difficult_computation << difficult_computation << difficult_computation;
 
     return sum;
 }
